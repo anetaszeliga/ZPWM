ed293783fc19fe93dd1bc43fc1c71395e206a3fa
aneta.szeliga <aneta.szeliga.95@gmail.com>
2018-03-09 11:18:17 +0100

    zmieniony lab02

---
+++ lab02/source/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <Windows.h>
+#include "res.h"
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) 
+{
+  HWND hwndMainWindow = CreateDialog(hinstance, MAKEINTRESOURCE(IDD_MAINVIEW),NULL,NULL);
+  ShowWindow(hwndMainWindow, iCmdShow);
+
+  MSG msg={};
+  while(GetMessage(&msg, NULL,0,0))
+  {
+  TranslateMessage(&msg);
+  DispatchMessage(&msg);
+  }
+  return 0;
+}
