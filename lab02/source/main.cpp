930dc47302da1c1f5921b3f9666d2f47ff581c55
aneta.szeliga <aneta.szeliga.95@gmail.com>
2018-03-12 09:07:34 +0100

    lab 02 koncowe zmiany

--- lab02/source/main.cpp
+++ lab02/source/main.cpp
@@ -4,6 +4,24 @@ INT_PTR CALLBACK DialogProc (HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPar
 {
 switch(uMsg)
 {
+case WM_COMMAND:
+switch (HIWORD(wParam))
+{
+case BN_CLICKED:
+  switch(LOWORD(wParam))
+  {
+  case IDC_BUTTON1:
+    MessageBox(hwndDlg,TEXT("Test"),TEXT("Klikniecie"),MB_OK);
+    HWND hwndEditBox=GetDlgItem(hwndDlg, IDC_EDIT1);
+    int iTextLength=GetWindowTextLength(hwndEditBox);
+    CHAR szText[500];
+    GetWindowText(hwndEditBox,szText,iTextLength+1);
+    SetWindowText((HWND)lParam,szText);
+    return TRUE;
+  }
+  
+}
+return FALSE;
 case WM_LBUTTONDOWN:
     CHAR szText[200];
     wsprintf(szText, "Kliknales myszka x=%d,y=%d",LOWORD(lParam),HIWORD(lParam));
